a21d68f0cc53fac47b14584917070867cf0121e5
gramos93 <piscina.ramos@gmail.com>
2019-12-18 09:38:48 -0500

    Added exer

--- exe_2_6.cpp
+++ exe_2_6.cpp
@@ -3,18 +3,20 @@
 
 int main(int argv, char *argc[])
 {
-    double x_prev, x_next;
+    double x_prev, x_next, ep = 1e-5;
 
     // Initial guess.
     x_prev = 0;
 
-    for (int i = 1; i < 100; i++)
+    do
     {
+        x_prev = x_next;
         x_next = x_prev - (exp(x_prev) + pow(x_prev, 3) - 5) /
                         (exp(x_prev) + 3 * pow(x_prev, 2));
         
-        printf("x_[%i] is %.10f\n", i, x_prev);
-    }
+        printf("x_next is %.10f\n", x_next);
+        
+    } while (fabs(x_next - x_prev) > ep);
 
     return 0;
 }
